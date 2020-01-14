34d784f2021564d621d13394dc6003273c95157e
David Manyà <david.manya@gmail.com>
2020-01-14 11:52:51 +0100

    Pujada FASE 1

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fase 1
+            char[] lletresnom = { 'D', 'A', 'V', 'I', 'D', 'M', 'A', 'Ñ', 'Á', 'Á', 'L', 'V', 'A', 'R', 'E', 'Z' };
+            for (int i = 0; i < lletresnom.Length; i++)
+            {
+                Console.WriteLine(lletresnom[i]);
+            }
+        }
+    }
+}
